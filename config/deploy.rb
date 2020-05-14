0071965978566b1ae3f127d1f2d7e3a126c0a0cb
Fukuda57 <joichiro.57.f@icloud.com>
2020-05-14 15:32:17 +0900

    Add-s3-function-fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,7 @@ set :application, 'chat-space'
 set :repo_url,  'git@github.com:Fukuda57/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
-
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
@@ -26,6 +25,7 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 set :linked_files, %w{ config/secrets.yml }
+
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
